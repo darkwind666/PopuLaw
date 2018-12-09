85b3a18d7f0af5ab45e76f078b7f8d3807816980
darkwind666 <darkwinddev@gmail.com>
2018-12-09 11:26:19 +0300

    make 13 screen

---
+++ PopuLaw/PopuLaw/Controllers/NoGulityViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NoGulityViewController.swift
+//  PopuLaw
+//
+//  Created by user on 12/9/18.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import UIKit
+
+class NoGulityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
+    }
+
+}
---
+++ PopuLaw/PopuLaw/Controllers/WhileGAEArriveViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WhileGAEArriveViewController.swift
+//  PopuLaw
+//
+//  Created by user on 12/9/18.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import UIKit
+
+class WhileGAEArriveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    @IBAction func backPressed(_ sender: Any) {
+        if let navController = self.navigationController {
+            navController.popViewController(animated: true)
+        }
+    }
+    
+    @IBAction func yesPressed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let subscriptionyVC = storyboard.instantiateViewController(withIdentifier: "YesGulityViewController") as! YesGulityViewController
+        self.navigationController?.pushViewController(subscriptionyVC, animated: true)
+    }
+    
+    @IBAction func noPressed(_ sender: Any) {
+        
+    }
+}
